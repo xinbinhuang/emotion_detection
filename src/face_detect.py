9aed0f48a919d49c06aeb52fc1e0dd7d75c7a6ba
Xinbin Huang <xbhuang93@hotmail.com>
2018-10-16 21:16:32 -0700

    Fix styling

--- src/face_detect.py
+++ src/face_detect.py
@@ -1,26 +1,26 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 import os
-import numpy as np
 import cv2
+import numpy as np
 
 
 def detect(img, cascade):
-     rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4,
-                                      minSize=(30, 30), flags=cv.CASCADE_SCALE_IMAGE)
-     if len(rects) == 0:
-         return []
-     # get (x1, y1), (x2, y2) = (x1, y1), (x1 + w, y1 + h)
-     rects[:, 2:] += rects[:, :2]
-     return rects
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4,
+                                     minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    # get (x1, y1), (x2, y2) = (x1, y1), (x1 + w, y1 + h)
+    rects[:, 2:] += rects[:, :2]
+    return rects
 
 
-def draw_rects(img, rects, color = (255, 0, 0)):
+def draw_rects(img, rects, color=(255, 0, 0)):
     for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1,y1), (x2,y2), color, 2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
 
-def draw_str(img, str, position, color = (255, 255, 0)):
+def draw_str(img, str, position, color=(255, 255, 0)):
     cv2.putText(img, str, position,
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                 color, 1, cv2.LINE_AA)
@@ -49,9 +49,9 @@ if __name__ == "__main__":
                     roi_gray = gray[y1:y2, x1:x2]
                     roi_vis = vis[y1:y2, x1:x2]
                     eyes = detect(roi_gray, eye_cascade)
-                    draw_rects(roi_vis, eyes, (255, 0 ,0))
+                    draw_rects(roi_vis, eyes, (255, 0, 0))
 
-            draw_str(vis, "Press 'q' to exit", (10,20), (255, 255, 0))
+            draw_str(vis, "Press 'q' to exit", (10, 20), (255, 255, 0))
             cv2.imshow("Face Detection", vis)
 
             if cv2.waitKey(10) & 0xFF == ord('q'):
